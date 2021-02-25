0f0378f4b1b3d4c3ecfb44c908b5fb5542907c9b
Sunyoring <sunhyo67@gmail.com>
2021-02-25 19:00:52 +0900

    Update Calendar

--- src/sunyo/calendar/Calendar.java
+++ src/sunyo/calendar/Calendar.java
@@ -1,36 +1,43 @@
 package sunyo.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private static final int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDaysOfMonth(int month) {
+	public static int getMaxDaysOfMonth(int month) {
 		return maxDays[month - 1];
 	}
 
-	public void printSampleCalendar() {
-
-		System.out.println("일   월   화   수   목   금   토 ");
-		System.out.println("--------------------------------");
-		System.out.println(" 1    2   3    4   5    6     7");
-		System.out.println(" 8   9   10  11   12  13   14");
-		System.out.println("15  16  17  18   19  20  21");
-		System.out.println("22  23  24  25   26  27  28");
-
+	public void printCalendar(int year, int month) {
+		System.out.printf("   << %4d년%3d월 >>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA ");
+		System.out.println("----------------------");
+		
+		int maxday = getMaxDaysOfMonth(month);
+		for(int i =1; i <=maxday; i++) {
+			System.out.printf("%3d",i);
+			if((i % 7) == 0) {
+				
+				System.out.println();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
+		printCalendar(2020, 3);
+	}
+}
 
-		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		String PROMPT = "cal> ";
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		System.out.println("반복횟수를 입력하세요");
-		int repeat = scanner.nextInt();
-		
+//System.out.println(" 1    2   3    4    5    6     7");
+//System.out.println(" 8   9   10  11   12  13  14");
+//System.out.println("15  16  17  18  19  20  21");
+//System.out.println("22  23  24  25  26  27  28");		
 
+// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("반복횟수를 입력하세요");
+//		int repeat = scanner.nextInt();
 
 //		for (int i=0; i < repeat; i++) {
 //			System.out.println("달을 입력하세요");
@@ -40,25 +47,4 @@ public class Calendar {
 //		}
 //		System.out.println("END.");
 //		
-		//while(true)는 무한루프
-		int month;
-		
-		while(true) {
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();			
-			
-			if (month <1) { 
-				System.out.println("숫자가 1보다 작으면 반복이 종료됩니다. ");
-				break;}
-			// break를 만나면 반복이 종료된다.
-			if (month > 12) {
-				System.out.println("12이하의 숫자만 입력하세요. ");
-				continue;}
-			// continue는 처음으로 되돌아간다.
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
-			
-		}
-		scanner.close();
-	}
-}
+// while(true)는 무한루프
---
+++ src/sunyo/calendar/Prompt.java
@@ -0,0 +1,43 @@
+package sunyo.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal > ";
+
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month;
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+
+			if (month < 1) {
+				System.out.println("숫자가 1 보다 작으면 종료됩니다. ");
+				break; 			// break를 만나면 반복이 종료된다.
+			}
+
+			if (month > 12) {
+				System.out.println("12 이하의 숫자만 입력하세요. ");
+				continue; 			// continue는 처음으로 되돌아간다.		
+			}
+				cal.printCalendar(2020, month);
+				System.out.println();
+				System.out.println();
+
+		}
+		System.out.println("bye~");
+		scanner.close();
+	}
+
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+	}
+
+}
