f6b5032e4abe67dc8f4c4d71c65ba5fd330e9673
Sunyoring <sunhyo67@gmail.com>
2021-02-28 23:13:17 +0900

    Update

--- src/sunyo/calendar/Prompt.java
+++ src/sunyo/calendar/Prompt.java
@@ -12,6 +12,7 @@ public class Prompt {
 
 		int month;
 		int year;
+		String weekday;
 		while (true) {
 			System.out.println("년도를 입력하세요");
 			System.out.print("YEAR > ");
@@ -19,6 +20,9 @@ public class Prompt {
 			System.out.println("달을 입력하세요");
 			System.out.print("MONTH > ");
 			month = scanner.nextInt();
+			System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, th, fr, sa).");
+			System.out.print("WEEKDAY > ");
+			weekday = scanner.next();
 
 			if (month < 1) {
 				System.out.println("숫자가 1 보다 작으면 종료됩니다. ");
@@ -30,6 +34,7 @@ public class Prompt {
 				continue; 			// continue는 처음으로 되돌아간다.		
 			}
 				cal.printCalendar(year, month);
+				System.out.println();
 				System.out.println();
 				System.out.println();
 				System.out.println();
