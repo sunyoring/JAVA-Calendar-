a984cdef099ad9889f106378a153d82c02e00abf
Sunyoring <sunhyo67@gmail.com>
2021-03-04 13:25:29 +0900

    Code Upgrade and Simplification

--- src/sunyo/calendar/Calendar2.java
+++ src/sunyo/calendar/Calendar2.java
@@ -1,6 +1,6 @@
 package sunyo.calendar;
 
-public class Calendar {
+public class Calendar2 {
 
 	private static final int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAPmaxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -20,6 +20,27 @@ public class Calendar {
 		}
 	}
 
+	public int getWeek(int year, int month, int day) {
+		int sYear = 1970;
+		int sWeek = 4;
+		int count = 0;
+
+		for (int i = sYear; i < year; i++) {
+			int delimiter = isLeapYear(i) ? 366 : 365;
+			count = count + delimiter;
+		}
+
+		for (int i = 1; i < month; i++) {
+			int delimiter = getMaxDaysOfMonth(year, i);
+			count = count + delimiter;
+		}
+		count = count + day - 1;
+
+		int weekday = (sWeek + count) % 7;
+
+		return weekday;
+	}
+
 	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   << %4d년%3d월 >>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA ");
@@ -31,36 +52,21 @@ public class Calendar {
 
 		int maxday = getMaxDaysOfMonth(year, month);
 
+		// 삼항연산자 조건문을 통해 두번째줄부터의 출력을 구현함.
+
 		int count = 7 - weekday;
+		int deli = (count < 7) ? count : 0;
 		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
 		}
 		System.out.println();
-		if (weekday == 0) {
-			for (int j = count + 1; j <= maxday; j++) {
-				System.out.printf("%3d", j);
-				if (j % 7 == 0) {
-					System.out.println();
-				}
+
+		for (int j = count + 1; j <= maxday; j++) {
+			System.out.printf("%3d", j);
+
+			if (j % 7 == deli) {
+				System.out.println();
 			}
-		}
-		// switch문을 이용하여 두번째 줄부터의 출력을 구현함.
-		switch (weekday) {
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
-			for (int j = count + 1; j <= maxday; j++) {
-				System.out.printf("%3d", j);
-
-				if (j % 7 == count) {
-					System.out.println();
-				}
-
-			}
-
 		}
 	}
 
--- src/sunyo/calendar/Prompt2.java
+++ src/sunyo/calendar/Prompt2.java
@@ -4,55 +4,31 @@ import java.util.Scanner;
 
 public class Prompt2 {
 
-	public int parseDay(String str_weekday) {
-		if (str_weekday.equals("su"))
-			return 0;
-		else if (str_weekday.equals("mo"))
-			return 1;
-		else if (str_weekday.equals("tu"))
-			return 2;
-		else if (str_weekday.equals("wd"))
-			return 3;
-		else if (str_weekday.equals("th"))
-			return 4;
-		else if (str_weekday.equals("fr"))
-			return 5;
-		else if (str_weekday.equals("sa"))
-			return 6;
-		else
-			return 0;
-	}
 
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
+		Calendar2 cal = new Calendar2();
 
 		int month;
 		int year;
-		int weekday;
-
 		while (true) {
 			System.out.println("년도를 입력하세요");			
 			System.out.print("YEAR > ");
 			year = scanner.nextInt();
+			
+			if (year < 1)
+				break ;
+						
 			System.out.println("달을 입력하세요");			
 			System.out.print("MONTH > ");
 			month = scanner.nextInt();
-			System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, wd, th, fr, sa).");			
-			System.out.print("WEEKDAY > ");
-			String str_weekday = scanner.next();
-			weekday = parseDay(str_weekday);
-
-			if (month < 1) {
-				System.out.println("숫자가 1 보다 작으면 종료됩니다. ");
-				break; // break를 만나면 반복이 종료된다.
-			}
-
-			if (month > 12) {
+			
+			if (month < 1 || month > 12) {
 				System.out.println("12 이하의 숫자만 입력하세요. ");
 				continue; // continue는 처음으로 되돌아간다.
 			}
-			cal.printCalendar(year, month, weekday);
+	
+			cal.printCalendar(year, month, cal.getWeek(year, month, 1));
 			System.out.println();
 		
 
